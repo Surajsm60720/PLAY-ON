8fea9409c6c02ddac93bbcfa009c8461be5c2501
MemestaVedas <kushalnayaksp@gmail.com>
2025-12-14 12:02:53 +0530

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import CurvedLoop from "./components/CurvedLoop";
+import ClickSpark from "./components/ClickSpark";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">
-      <h1>Welcome to PLAY-ON!</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Inspired from Taiga, this app aims to provide a simple and user-friendly interface for watching anime locally and updating them on AniList.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
+    <>
+      <ClickSpark
+        sparkColor='#fff'
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+        <CurvedLoop marqueeText="Welcome to Play-ON!✦" />
+      </ClickSpark>
+    </>
   );
 }
 
